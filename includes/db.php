7b45b0b337ae39dd3234b6f9ef3143e6f00e5313
Dominic Lacroix <supercoderdom@gmail.com>
2018-05-10 10:44:22 -0700

    use dotenv variables to secure database connection

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,5 @@
 <?php 
 
-    // $db['db_host'] = 'localhost';
-    // $db['db_username'] = 'root';
-    // $db['db_password'] = 'c0ding!5fun';
-    // $db['db_database'] = 'cms';
-
-    // foreach($db as $key => $value) {
-    //     define(strtoupper($key), $value);
-    // }
-
     $connection = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE']);
 
     if(!$connection) {
