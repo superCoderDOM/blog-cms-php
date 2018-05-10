8532f75bd934436e843a022f3946544eb6e65d5d
Dominic Lacroix <supercoderdom@gmail.com>
2018-05-10 10:40:54 -0700

    require autoload of composer packages and import dotenv variables

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php 
+
+    require __DIR__ . '/vendor/autoload.php';
+
+
+    $dotenv = new Dotenv\Dotenv(__DIR__);
+    $dotenv->load();
+    $dotenv->required(['DB_HOST', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD']);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
