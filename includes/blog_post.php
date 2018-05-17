e01a85d88fe6efe45342d39fb6ef897edcc9bdf5
Dominic Lacroix <supercoderdom@gmail.com>
2018-05-17 16:35:41 -0700

    add logic to display comments below blog posts

--- includes/blog_post.php
+++ includes/blog_post.php
@@ -23,36 +23,36 @@
 
             <!-- Blog Post Content Column -->
             <div class="col-lg-8">
-            
+
                 <!-- Blog Post -->
-            
+
                 <!-- Title -->
                 <h1><?php echo $post_title ?></h1>
-            
+
                 <!-- Author -->
                 <p class="lead">
                     by <a href="#"><?php echo $post_author; ?></a>
                 </p>
-            
+
                 <hr>
-            
+
                 <!-- Date/Time -->
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo date('F j, Y \a\t g:i A', $post_date); ?></p>
-            
+
                 <hr>
-            
+
                 <!-- Preview Image -->
                 <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="featured blog post image">
-            
+
                 <hr>
-            
+
                 <!-- Post Content -->
                 <p><?php echo $post_content; ?></p>
-            
+
                 <hr>
-            
+
                 <!-- Blog Comments -->
-            
+
                 <!-- Comments Form -->
 
                 <?php 
@@ -96,24 +96,46 @@
                         <button type="submit" class="btn btn-primary" name="submit_comment"> Submit </button>
                     </form>
                 </div>
-            
+
                 <hr>
-            
+
                 <!-- Posted Comments -->
-            
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    </div>
-                </div>
-            
+
+                <?php 
+
+                $query = "SELECT * FROM comments 
+                          WHERE comment_post_id = '{$post_id}' 
+                          AND comment_status = 'Approved' 
+                          ORDER BY comment_id DESC";
+
+                $selectAllComments = mysqli_query($connection, $query);
+
+                    while($row = mysqli_fetch_assoc($selectAllComments)) {
+
+                        $comment_author = $row['comment_author'];
+                        $comment_content = $row['comment_content'];
+                        $comment_date = strtotime($row['comment_date']);
+
+                        ?>
+
+                        <!-- Comment -->
+                        <div class="media">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" src="http://placehold.it/64x64" alt="">
+                            </a>
+                            <div class="media-body">
+                                <h4 class="media-heading"><?php echo $comment_author; ?>
+                                    <small><?php echo date('F j, Y \a\t g:i A', $post_date); ?></small>
+                                </h4>
+                                <?php echo $comment_content; ?>
+                            </div>
+                        </div>
+
+                        <?php 
+                    }
+                ?>
+
+
                 <!-- Comment -->
                 <div class="media">
                     <a class="pull-left" href="#">
