354fae7fc33f3861da0faeda6dae826e8edee232
Dominic Lacroix <supercoderdom@gmail.com>
2018-05-17 16:08:24 -0700

    remove switch statement

--- admin/includes/admin_comments.php
+++ admin/includes/admin_comments.php
@@ -4,27 +4,7 @@
 
         <h1 class="page-header"> Comments </h1>
 
-        <?php 
-
-            if(isset($_GET['source'])) {
-                $source = $_GET['source'];
-            } else {
-                $source = '';
-            }
-
-            // Display only requested posts
-            switch($source) {
-                case 'approve_comment' :
-                    include 'includes/forms/approve_comment.php';
-                    break;
-                case 'unapprove_comment' :
-                    include 'includes/forms/reject_comment.php';
-                    break;
-                default :
-                    include 'includes/view_all_comments.php';
-            }
-
-        ?>
+        <?php include 'includes/view_all_comments.php'; ?>
 
     </div>
     <!-- /.col-lg-12 -->
