54d994b58e81718aa259b03104873da5e5d42b8f
Dominic Lacroix <supercoderdom@gmail.com>
2018-05-17 12:01:11 -0700

    change file name, add links to access individual post pages

--- includes/blog_list.php
+++ includes/blog_list.php
@@ -7,6 +7,7 @@
     while($row = mysqli_fetch_assoc($allPosts)) {
 
         // Save required fields to variables
+        $post_id = $row['post_id'];
         $post_title = $row['post_title'];
         $post_author = $row['post_author'];
         $post_date =  strtotime($row['post_date']);
@@ -17,14 +18,16 @@
         ?>
         
         <h2>
-            <a href="#"><?php echo $post_title; ?></a>
+            <a href="post.php?post_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
         </h2>
         <p class="lead">
-            by <a href="index.php"><?php echo $post_author; ?></a>
+            by <a href="index.php?author_id=$post_author_id"><?php echo $post_author; ?></a>
         </p>
         <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo date('F j, Y \a\t g:i A', $post_date); ?></p>
         <hr>
-        <img class="img-responsive" src="images/<?php echo $post_image; ?> " alt="">
+        <a href="post.php?post_id=<?php echo $post_id; ?>">
+            <img class="img-responsive" src="images/<?php echo $post_image; ?> " alt="">
+        </a>
         <hr>
         <p><?php echo $post_content; ?></p>
         <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
