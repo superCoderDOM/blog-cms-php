7764014fedcaaeb88c2ecfeda922a71d1968a52e
Dominic Lacroix <supercoderdom@gmail.com>
2018-05-17 17:25:48 -0700

    minor format changes

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -1,16 +1,16 @@
 <table class="table table-bordered table-hover">
     <thead class="thead-dark">
         <tr>
-            <th>Id</th>
-            <th>Blog Post</th>
-            <th>Author</th>
-            <th>Email</th>
-            <th>Comment</th>
-            <th>Status</th>
-            <th>Date</th>
-            <th>Approve</th>
-            <th>Reject</th>
-            <th>Delete</th>
+            <th> Id </th>
+            <th> Blog Post </th>
+            <th> Author </th>
+            <th> Email </th>
+            <th> Comment </th>
+            <th> Status </th>
+            <th> Date </th>
+            <th> Approve </th>
+            <th> Reject </th>
+            <th> Delete </th>
         </tr>
     </thead>
     <tbody>
--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,15 +1,17 @@
 <table class="table table-bordered table-hover">
     <thead class="thead-dark">
         <tr>
-            <th>Id</th>
-            <th>Author</th>
-            <th>Title</th>
-            <th>Category</th>
-            <th>Status</th>
-            <th>Image</th>
-            <th>Tags</th>
-            <th>Comments</th>
-            <th>Date</th>
+            <th> Id </th>
+            <th> Author </th>
+            <th> Title </th>
+            <th> Category </th>
+            <th> Status </th>
+            <th> Image </th>
+            <th> Tags </th>
+            <th> Comments </th>
+            <th> Date </th>
+            <th> Edit </th>
+            <th> Delete </th>
         </tr>
     </thead>
     <tbody>
