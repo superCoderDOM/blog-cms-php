ea0212fa3a45d32125934b337602d1fe3e1ddaa3
Dominic Lacroix <supercoderdom@gmail.com>
2018-05-29 11:13:40 -0700

    change admin condition spelling

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -14,11 +14,11 @@
     include "functions.php";
 
     if(isset($_SESSION['user_role'])) {
-        if($_SESSION['user_role'] !== 'admin') {
+        if($_SESSION['user_role'] !== 'Admin') {
             header("Location: ../index.php");
         }
-    } else {
-        header("Location: ../index.php");
+    // } else {
+        // header("Location: ../index.php");
     }
 
 ?>
